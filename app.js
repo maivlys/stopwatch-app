cec7d6ac176966ecdfb9a9ef072fdd6007e0945d
maivlys <122049862+maivlys@users.noreply.github.com>
2024-02-25 01:07:56 +0100

    adds all functionality
    description

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
 const startBtn = document.querySelector(".js-start-btn");
 const stopBtn = document.querySelector(".js-stop-btn");
 const resetBtn = document.querySelector(".js-reset-btn");
-const timerDisplay = document.querySelector(".js-timer-display");
 
-startBtn.addEventListener("click", startTimer);
-// stopBtn.addEventListener("click");
+const hoursHTML = document.querySelector(".js-hours");
+const minutesHTML = document.querySelector(".js-minutes");
+const secondsHTML = document.querySelector(".js-seconds");
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let intervalId;
+
+startBtn.addEventListener(
+  "click",
+  () => (intervalId = setInterval(counting, 1000))
+);
+stopBtn.addEventListener("click", () => clearInterval(intervalId));
 resetBtn.addEventListener("click", resetTimer);
 
-function startTimer() {
-  timerDisplay.innerHTML = "00:00:01";
+function counting() {
+  // if (seconds < 59) {
+  //   seconds++;
+  // } else {
+  //   seconds = 0;
+  //   if (minutes < 59) {
+  //     minutes++;
+  //   } else {
+  //     minutes = 0;
+  //     hours++;
+  //   }
+  // }
+
+  if (seconds < 59) {
+    seconds++;
+  } else if (minutes < 59) {
+    seconds = 0;
+    minutes++;
+  } else {
+    seconds = 0;
+    minutes = 0;
+    hours++;
+  }
+
+  secondsHTML.innerHTML = String(seconds).padStart(2, "0");
+  minutesHTML.innerHTML = String(minutes).padStart(2, "0");
+  hoursHTML.innerHTML = String(hours).padStart(2, "0");
 }
 
-// function stopTimer() {}
-
 function resetTimer() {
-  timerDisplay.innerHTML = "00:00:00";
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  hoursHTML.innerHTML = "00";
+  minutesHTML.innerHTML = "00";
+  secondsHTML.innerHTML = "00";
 }
